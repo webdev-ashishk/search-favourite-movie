3025e59adfd30b85be30d332b0ebc61b2e92b300
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-14 13:06:54 +0530

    video #3 completed like sir code

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import Home from "./Component/Home";
 import { Routes, Route } from "react-router-dom";
 import SingleMovie from "./Component/SingleMovie";
 import Error from "./Component/Error";
-import { AppContext } from "./Component/Context";
+
 function App() {
   return (
     <>
-      <AppContext.Provider value="AshishK">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="movie/:id" element={<SingleMovie />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </AppContext.Provider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<SingleMovie />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </>
   );
 }
--- src/Component/Context.jsx
+++ src/Component/Context.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 
 const AppContext = React.createContext();
-
+// const API_URL = `http://www.omdbapi.com/?apikey=6f605eb6&s=hacker`;
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value="AshishKK">{children}</AppContext.Provider>;
+};
 // global custom hook
 const useGlobalContext = () => {
   return useContext(AppContext);
 };
-export { AppContext, useGlobalContext };
+export { AppContext, useGlobalContext, AppProvider };
