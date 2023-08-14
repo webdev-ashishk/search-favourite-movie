fc5ff03e0eb998f2ae44db37994b4fa32dc490f6
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-14 15:07:22 +0530

    Video #5 completed'

--- src/Component/Movies.jsx
+++ src/Component/Movies.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { useGlobalContext } from "./Context";
 
 const Movies = () => {
@@ -5,15 +6,26 @@ const Movies = () => {
   return (
     <>
       <div>Movies</div>
-      {movie.map((currentMovie) => {
-        return (
-          <>
-            <div>
-              <h3>{currentMovie.Title}</h3>
-            </div>
-          </>
-        );
-      })}
+      <section className="movie-page">
+        <div className="container grid grid-4-col">
+          {movie.map((currentMovie) => {
+            const { imdbID, Poster, Title, Year } = currentMovie;
+            return (
+              <>
+                <NavLink to={`movie/${imdbID}`} key={imdbID}>
+                  <div className="cart">
+                    <div className="cart-info">
+                      <h2>{Title}</h2>
+                      <img src={Poster} alt={imdbID} />
+                      <h5>Year:{Year}</h5>
+                    </div>
+                  </div>
+                </NavLink>
+              </>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 };
