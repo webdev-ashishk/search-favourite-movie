134fdfeee1f8404dda95688fb3d7fcba99df31d4
webdev-ashishk <webdev.ashishk@gmail.com>
2023-08-14 09:32:33 +0530

    Vidoe #1 completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./Home";
+import Movies from "./Movies";
+import Search from "./Search";
+function App() {
+  return (
+    <>
+      <div>app.jsx</div>
+      <Home />
+      <Search />
+      <Movies />
+    </>
+  );
+}
+
+export default App;
